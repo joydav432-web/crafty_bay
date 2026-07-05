118dd09d8d612fdce0fc29843184477c679c52bb
my <joydav432@gmail.com>
2026-07-05 16:08:31 +0600

    network response add

--- lib/core/service/network_caller/network_caller.dart
+++ lib/core/service/network_caller/network_caller.dart
@@ -3,38 +3,101 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:logger/logger.dart';
 
+import 'network_response.dart';
+
 class NetworkCaller{
 
   final Logger _logger = Logger();
+  final Map<String,String> headers;
+
+  NetworkCaller({required this.headers});
 
   Future <NetworkResponse> getRequest(String url)async{
 
-    final Uri uri = Uri.parse(url);
-    _logRequest(url);
-    final Response response = await get(uri);
-    _logResponse(response);
+    try {
+      final Uri uri = Uri.parse(url);
+      _logRequest(url,headers: headers);
+      final Response response = await get(uri,headers: headers);
+      _logResponse(response);
 
 
 
-    if(response.statusCode == 200){
+      if(response.statusCode == 200){
 
-      final decodedJson = jsonDecode(response.body);
+        final decodedJson = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            body: decodedJson
+        );
+      }else{
+        final decodedJson = jsonDecode(response.body);
+
+
+        return NetworkResponse(
+        isSuccess: false,
+            statusCode: response.statusCode,
+           errorMessage: decodedJson['message']??'Something went wrong'
+        );
+      }
+    } on Exception catch (e) {
+      // TODO
+
       return NetworkResponse(
-          isSuccess: true,
-          statusCode: response.statusCode,
-          body: decodedJson
-      );
-    }else{
-      final decodedJson = jsonDecode(response.body);
+          isSuccess:false,
+        statusCode: -1,
+        errorMessage: e.toString()
 
-
-      return NetworkResponse(
-      isSuccess: false,
-          statusCode: response.statusCode,
-         errorMessage: decodedJson['message']??'Something went wrong'
       );
     }
   }
+
+
+
+  Future <NetworkResponse> postRequest(String url,{Map<String,dynamic>? body})async{
+
+    try {
+      final Uri uri = Uri.parse(url);
+      _logRequest(url,requestBody: body,headers: headers);
+      final Response response = await post(uri,headers:headers,
+
+          body:jsonEncode(body));
+      _logResponse(response);
+
+
+
+      if(response.statusCode == 200||response.statusCode==201){
+
+        final decodedJson = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            body: decodedJson
+        );
+      }else{
+        final decodedJson = jsonDecode(response.body);
+
+
+        return NetworkResponse(
+            isSuccess: false,
+            statusCode: response.statusCode,
+            errorMessage: decodedJson['message']??'Something went wrong'
+        );
+      }
+    } on Exception catch (e) {
+      // TODO
+
+      return NetworkResponse(
+          isSuccess:false,
+          statusCode: -1,
+          errorMessage: e.toString()
+
+      );
+    }
+  }
+
+
+
 
   void _logRequest(String url,{Map<String,dynamic>? requestBody,Map<String,String>? headers}){
 
@@ -71,17 +134,3 @@ class NetworkCaller{
 
 }
 
-class NetworkResponse{
-
-  final bool isSuccess;
-  final int statusCode;
-  final dynamic body;
-  final String? errorMessage;
-
-  NetworkResponse({
-    required this.isSuccess,
-    required this.statusCode,
-    this.body,
-    this.errorMessage
-  });
-}
---
+++ lib/core/service/network_caller/network_response.dart
@@ -0,0 +1,14 @@
+class NetworkResponse {
+
+  final bool isSuccess;
+  final int statusCode;
+  final dynamic body;
+  final String? errorMessage;
+
+  NetworkResponse({
+    required this.isSuccess,
+    required this.statusCode,
+    this.body,
+    this.errorMessage
+  });
+}
